e56f102a7e01ac8bb7aa9117a4ce09c0f63feb3a
Gokul R <srigokulrajendiran@gmail.com>
2021-09-12 02:30:25 +0530

    '-'

---
+++ index.js
@@ -0,0 +1,14 @@
+var acc = document.getElementsByClassName("accordion01");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active01");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
