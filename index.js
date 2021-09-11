b544a6eed8e2261243597ee37e592c7401570e34
Gokul R <srigokulrajendiran@gmail.com>
2021-09-12 02:34:05 +0530

    first commit

--- index.js
+++ index.js
@@ -12,3 +12,9 @@ for (i = 0; i < acc.length; i++) {
         }
     });
 }
+/* git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https: //github.com/gokul2507/inter.git
+git push -u origin main*/
